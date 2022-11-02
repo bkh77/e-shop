11aaadb2f1250f8f4f5414ffa22187f987e88808
Bakhtiyor Khasanov <bkhasanov77@gmail.com>
2022-11-02 17:10:30 +0500

    [e-shop] created nav component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { FaUserCircle } from 'react-icons/fa'
+import { HiBell } from 'react-icons/hi'
+
+function NavBar() {
+  return (
+    <nav className='max-w-full bg-white h-[70px] flex'>
+      <div className='flex justify-between items-center w-full'>
+        <div className='ml-16 text-lg'>LOGO</div>
+        <div className='relative'>
+          <HiBell className='text-gray w-6 h-6 mr-10' />
+          <div className='w-4 h-4 bg-green rounded-full text-white flex justify-center items-center text-[10px] absolute -top-1 left-3'>
+            1
+          </div>
+        </div>
+      </div>
+
+      <div className='bg-blue h-[70px] w-[292px] rounded-bl-[30px] text-white flex justify-center items-center space-x-4'>
+        <span>Личный кабинет</span>
+        <FaUserCircle className='w-8 h-8' />
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+    colors: {
+      blue: '#3D639D',
+      white: '#ffffff',
+      gray: '#BBC2D0',
+      green: {
+        DEFAULT: '#00C48C',
+      },
+    },
+  },
+  plugins: [],
+}
