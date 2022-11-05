2972506fab98741fc6c85246624822887b8eae7e
Bakhtiyor Khasanov <bkhasanov77@gmail.com>
2022-11-05 19:08:19 +0500

    [e-shop] added responsive features

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -6,17 +6,17 @@ function SideBar() {
   const href = useHref()
   const [activeMenu, setActiveMenu] = useState(href)
   return (
-    <aside className='w-[263px] bg-blue rounded-tr-[30px] text-white pl-8 py-8'>
+    <aside className='md:w-[263px] w-16 bg-blue rounded-tr-[30px] text-white pl-2 md:pl-8 py-8'>
       {menus.map((menu) => (
         <Link to={menu.pathName} key={menu.title}>
           <div
             onClick={() => setActiveMenu(menu.pathName)}
-            className={`flex items-center space-x-4 py-4 pl-6 rounded-l-full cursor-pointer transition duration-150 ${
+            className={`flex items-center space-x-4 py-4 pl-2 md:pl-6 rounded-l-full cursor-pointer transition duration-150 ${
               activeMenu === menu.pathName ? 'active' : null
             }`}
           >
             <span>{menu.icon}</span>
-            <span>{menu.title}</span>
+            <span className='hidden md:block'>{menu.title}</span>
           </div>
         </Link>
       ))}
--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -6,12 +6,12 @@ import Card from '../components/Card'
 
 function Order() {
   return (
-    <div className='bg-gray-light rounded-md py-5 px-7'>
+    <div className='bg-gray-light rounded-md py-3 px-4 md:py-5 md:px-7'>
       <p className='text-gray text-[12px]'>
         Заявки <MdOutlineArrowForwardIos className='inline' />{' '}
         <span className='text-green'>Оформить заказ</span>
       </p>
-      <h3 className='text-blue text-[22px] leading-6 font-semibold mt-7'>
+      <h3 className='text-blue text-lg md:text-[22px] leading-6 font-semibold mt-7'>
         Оформить заказ
       </h3>
 
@@ -19,7 +19,7 @@ function Order() {
 
       <p className='text-black font-bold mb-6'>Все товары (121)</p>
 
-      <div className='grid grid-cols-4 gap-4'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 2xl:grid-cols-6 gap-4'>
         {data.map((phone) => (
           <Card key={phone.id} phone={phone} />
         ))}
